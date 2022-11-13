82d8e34167c97c831d51c31060b0d2f7ad491d2c
Carlos Leret <carlos.leret@orderyoyo.com>
2022-11-13 12:07:22 +0100

    add leflet libraries, render map

---
+++ src/components/MapFrame/MapFrame.tsx
@@ -0,0 +1,32 @@
+import { LatLngExpression } from "leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import { MapTool } from "./PathMarkers/PathMarkers";
+
+export const MapFrame = () => {
+  const [initialPoint, ] = useState<LatLngExpression>([
+    52.51028, 13.40789,
+  ]);
+
+  return (
+    <>
+      <MapContainer
+        center={initialPoint}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={initialPoint}>
+          <Popup>
+            Welcome to our Berlin office <br /> Enjoy the views.
+          </Popup>
+        </Marker>
+        {/* <MapTool /> */}
+      </MapContainer>
+    </>
+  );
+};
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { MapFrame } from '../components/MapFrame/MapFrame'
 import { PageLayout } from '../containers/PageLayout'
 
 export const MainPage = () => {
   return (
     <PageLayout>
-        Drone Flight Plannerrrr
+        <MapFrame />
     </PageLayout>
   )
 }
